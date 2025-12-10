2ad9ae52c66551a7b5793af4cbb6af0a2a429e6a
polluste888 <bert-robert.polluste@voco.ee>
2025-12-10 12:44:13 +0200

    react

---
+++ react/src/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+ return (
+    <header>
+        <h1>AYTON DOMINATOR</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li>Contact</li>
+                <li>contact</li>
+            </ul>
+        </nav>
+    </header>
+ );
+}
+
+export default Header
