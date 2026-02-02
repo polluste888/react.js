48cf476ec5f7882732423f58ea9f930dfab43b62
polluste888 <bert-robert.polluste@voco.ee>
2026-02-02 19:07:09 +0200

    ül6

--- react/src/components/ExpenseForm.jsx
+++ react/src/components/ExpenseForm.jsx
@@ -1,26 +1,55 @@
-import "./ExpenseForm.css";
+import React, { useState } from 'react';
+import './Expenseform.css';
 
 const ExpenseForm = (props) => {
-    return (
-        <form>
-        <div className="new-expense__controls">
-            <div className="new-expense__control">
-                <label>Title</label>
-                <input type="text" />
-            </div>
-            <div className="new-expense__control">
-                <label>Price</label>
-                <input type="number" min="0.01" step="0.01" />
-            </div>
-            <div className="new-expense__control">
-                <label>Date</label>
-                <input type="date" min="2024-01-01" max="2026-12-31" />
-            </div>
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredPrice, setEnteredPrice] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
+  const titleChangeHandler = (event) => setEnteredTitle(event.target.value);
+  const priceChangeHandler = (event) => setEnteredPrice(event.target.value);
+  const dateChangeHandler = (event) => setEnteredDate(event.target.value);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      price: +enteredPrice, 
+      date: new Date(enteredDate),
+    };
+
+    props.onSaveExpenseData(expenseData);
+    
+    
+    setEnteredTitle('');
+    setEnteredPrice('');
+    setEnteredDate('');
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
+          <label>Title</label>
+          <input type='text' value={enteredTitle} onChange={titleChangeHandler} required />
         </div>
-        <div className="new-expense__actions">
-            <button type="submit">Add Expense</button>
+        <div className='new-expense__control'>
+          <label>Price</label>
+          <input type='number' min='0.01' step='0.01' value={enteredPrice} onChange={priceChangeHandler} required />
         </div>
-        </form>
-    )
-}
+        <div className='new-expense__control'>
+          <label>Date</label>
+          <input type='date' min='2023-01-01' max='2026-12-31' value={enteredDate} onChange={dateChangeHandler} required />
+        </div>
+      </div>
+      <div className='new-expense__actions'>
+        
+        <button type="button" onClick={props.onCancel}>Cancel</button>
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
 export default ExpenseForm;
--- react/src/components/Newexpense.jsx
+++ react/src/components/Newexpense.jsx
@@ -1,11 +1,48 @@
-import ExpenseForm from "./ExpenseForm";
-const NewExpense = (props) => {
-    return (
-        <div className="new-expense">
-            <ExpenseForm />
-        </div>
-    )
+import React, { useState } from 'react';
+import ExpenseForm from './Expenseform'; 
+import './Newexpense.css';
 
-    }
+const NewExpense = (props) => {
+  // Vihje 1: Olek vormi näitamise haldamiseks
+  const [isEditing, setIsEditing] = useState(false);
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    
+    props.onAddExpense(expenseData);
+    
+    // Vihje 4: Pane vorm kinni pärast andmete edukat saatmist
+    setIsEditing(false);
+  };
+
+  // Vihje 2: Funktsioonid vormi avamiseks ja sulgemiseks
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
+  return (
+    <div className='new-expense'>
+      
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+
+     
+      {isEditing && (
+        <ExpenseForm 
+          onSaveExpenseData={saveExpenseDataHandler} 
+          onCancel={stopEditingHandler} 
+        />
+      )}
+    </div>
+  );
+};
 
 export default NewExpense;
